60b250b01f05fb016d2664dc4745901ed53376e1
Vittoria Scocco <scoccv@usi.ch>
2025-12-09 18:34:08 +0100

    Removed extra prepare messages

--- protocols/paxos_node.py
+++ protocols/paxos_node.py
@@ -17,9 +17,9 @@ class PaxosNode(Node):
     class AcceptMsg:
         def __init__(self, ballot, value):
             self.type = "ACCEPT"; self.ballot = ballot; self.value = value
-    class AcceptedMsg:
+    class LearnMsg:
         def __init__(self, acceptor_id, ballot, value):
-            self.type = "ACCEPTED"; self.id = acceptor_id; self.ballot = ballot; self.value = value
+            self.type = "LEARN"; self.id = acceptor_id; self.ballot = ballot; self.value = value
     class HeartbeatMsg:
         def __init__(self, leader_id, ballot):
             self.type = "HEARTBEAT"; self.leader_id = leader_id; self.ballot = ballot
@@ -33,17 +33,18 @@ class PaxosNode(Node):
         self.quorum_size = len(self.all_nodes) // 2 + 1
         
         # State
-        self.store.setdefault('promised_ballot', 0)
-        self.store.setdefault('accepted_prop', None)
+        self.store.setdefault('promised_ballot', 0) # highest-numbered prepare req to which it has responded 
+        self.store.setdefault('accepted_prop', None) # highest-numbered proposal it has ever accepted
         self.store.setdefault('commits', 0)
         
         self.is_leader = False
         self.current_leader = None
-        self.ballot = 0
+        # self.ballot = 0
+        self.ballot = self.id  # Start ballot with node ID to avoid conflicts
         
         self.potential_commands = [] 
         self.promises_received = {}
-        self.accepted_received = {}
+        self.learn_received = {}
         
         # Timers
         self.heartbeat_interval = 50.0
@@ -51,7 +52,7 @@ class PaxosNode(Node):
         self.reset_election_timer()
 
         # TODO: REMOVE
-        # self.clear_file_commands()
+        self.clear_file_commands()
 
 
     # TODO: REMOVE
@@ -76,11 +77,18 @@ class PaxosNode(Node):
         self.ballot += len(self.all_nodes) 
         self.promises_received[self.ballot] = []
         
-        msg = self.PrepareMsg(self.ballot)
+        # msg = self.PrepareMsg(self.ballot)
+        # for n in self.all_nodes:
+        #     # DUZELTME: self.net.send yerine self.send kullan
+        #     self.send(n, msg)
+        # self.reset_election_timer()
+
+    def broadcast_prepare(self, value):
+        """Helper to broadcast Prepare messages."""
+        msg = self.PrepareMsg(value)
         for n in self.all_nodes:
-            # DUZELTME: self.net.send yerine self.send kullan
+            # DUZELTME: self.send kullan
             self.send(n, msg)
-        self.reset_election_timer()
 
     def broadcast_accept(self, value):
         """Helper to broadcast Accept messages."""
@@ -125,14 +133,16 @@ class PaxosNode(Node):
             if msg.ballot not in self.promises_received: self.promises_received[msg.ballot] = []
             self.promises_received[msg.ballot].append(msg)
             
-            if len(self.promises_received[msg.ballot]) == self.quorum_size:
-                if self.potential_commands:
-                    val = self.potential_commands[0]
-                else:
-                    val = (-1, -1, f"noop_{self.ballot}")
-                
-                self.broadcast_accept(val)
-                self.set_timer(self.heartbeat_interval, "heartbeat_timer")
+            if len(self.promises_received[msg.ballot]) != self.quorum_size:
+                return
+
+            if self.potential_commands:
+                val = self.potential_commands[0]
+            else:
+                val = (-1, -1, f"noop_{self.ballot}")
+            
+            self.broadcast_accept(val)
+            self.set_timer(self.heartbeat_interval, "heartbeat_timer")
 
         # 4. ACCEPT (Acceptor Logic)
         elif mtype == "ACCEPT":
@@ -142,51 +152,62 @@ class PaxosNode(Node):
                 self.current_leader = src
                 self.reset_election_timer()
                 
-                reply = self.AcceptedMsg(self.id, msg.ballot, msg.value)
+                reply = self.LearnMsg(self.id, msg.ballot, msg.value)
                 for n in self.all_nodes:
                     # DUZELTME
                     self.send(n, reply)
 
-        # 5. ACCEPTED (Commit Logic)
-        elif mtype == "ACCEPTED":
-            key = (msg.ballot, msg.value)
-            if key not in self.accepted_received: self.accepted_received[key] = set()
-            self.accepted_received[key].add(msg.id)
-            
-            if len(self.accepted_received[key]) == self.quorum_size:
-                committed_val = msg.value
+        # 5. LEARN (Learner logic)
+        elif mtype == "LEARN":
 
-                print(f"✅ Node {self.id} COMMITTED value: {committed_val} at t={self.sim.time:.1f}ms")
+            # TODO: remove
+            # dont do anything for ballots > 100
+            if msg.ballot > 100:
+                pass
 
-                # TODO: REMOVE
-                # self.execute_command(committed_val[2])
+            prop = (msg.ballot, msg.value)
+            if prop not in self.learn_received: self.learn_received[prop] = set()
+            self.learn_received[prop].add(msg.id)
 
-                if committed_val in self.potential_commands:
-                    self.potential_commands.remove(committed_val)
-                    self.store['commits'] = self.store.get('commits', 0) + 1
-                    
-                    client_id, req_id, _ = committed_val
-                    if client_id >= 0:
-                        reply = {
-                            "type": "REPLY",
-                            "request_id": req_id,
-                            "status": "COMMITTED"
-                        }
-                        self.send(client_id, reply)
+            if len(self.learn_received[prop]) != self.quorum_size:
+                return
+
+            committed_val = msg.value
+
+            # TODO: REMOVE
+            self.execute_command(committed_val[2])
+
+            if committed_val in self.potential_commands:
+                self.potential_commands.remove(committed_val)
+                self.store['commits'] = self.store.get('commits', 0) + 1
+                
+                client_id, req_id, _ = committed_val
+                if client_id >= 0:
+                    reply = {
+                        "type": "REPLY",
+                        "request_id": req_id,
+                        "status": "COMMITTED"
+                    }
+                    self.send(client_id, reply)
 
         # 6. CLIENT REQUEST
         elif mtype == "REQUEST":
+
+            print(f"Node {self.id} received client request {msg['request_id']} at t={self.sim.time:.1f}ms")
+
             if self.is_leader:
                 cmd_tuple = (msg["client_id"], msg["request_id"], msg["data"])
                 if cmd_tuple not in self.potential_commands:
                     self.potential_commands.append(cmd_tuple)
                     self.broadcast_accept(cmd_tuple)
+                    # self.broadcast_prepare(cmd_tuple)
                     
             elif self.current_leader is not None:
                 # DUZELTME: Lidere yönlendir
                 self.send(self.current_leader, msg)
 
     def on_timer(self, timer_id):
+        print("THIS FIRED")
         if timer_id == "election_timer":
             if not self.is_leader: self.start_election()
             else: self.reset_election_timer()
